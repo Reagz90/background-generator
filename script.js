d5b82cdbdc65cc976e882f0c7da773e854127a6f
Reagz <44364513+Reagz90@users.noreply.github.com>
2023-06-13 20:05:11 +1000

    background generator ztm project

---
+++ script.js
@@ -0,0 +1,13 @@
+const css = document.querySelector("h3");
+const colour1 = document.querySelector(".colour1");
+const colour2 = document.querySelector(".colour2");
+const body = document.getElementById("gradient");
+
+function setGradient() {
+  body.style.background =
+    "linear-gradient(to right, " + colour1.value + ", " + colour2.value + ")";
+  css.textContent = body.style.background + ";";
+}
+
+colour1.addEventListener("input", setGradient);
+colour2.addEventListener("input", setGradient);
